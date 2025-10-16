30b4864834aea41840a468695baebe06ad744618
olamideakintayo <divineolamide977@gmail.com>
2025-10-16 10:15:36 +0100

    Added one to many relationship for product

--- src/main/java/org/kashcode/inventorymanagementsystem/data/models/Supplier.java
+++ src/main/java/org/kashcode/inventorymanagementsystem/data/models/Supplier.java
@@ -1,24 +1,28 @@
-package org.kashcode.inventorymanagementsystem.data.models;
+package com.souk.inventory.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
 
-    @Entity
-    @Table(name = "suppliers")
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public class Supplier {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long supplierId;
+@Entity
+@Table(name = "suppliers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Supplier {
 
-        @Column(nullable = false)
-        private String name;;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long supplierId;
 
-        @Column(nullable = false)
-        private String contactInformation;
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String contactInformation;
+
+    @OneToMany(mappedBy = "supplier", cascade = CascadeType.ALL)
+    private List<Product> suppliedProducts;
 }
-
