b100d136cecaa09e296bfab6ae7e5241f161aecf
olamideakintayo <divineolamide977@gmail.com>
2025-10-16 10:40:55 +0100

    added the Product model

---
+++ src/main/java/org/kashcode/inventorymanagementsystem/data/models/Product.java
@@ -0,0 +1,34 @@
+package org.kashcode.inventorymanagementsystem.data.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "products")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String productId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer reOrderThreshold;
+
+    @Column(nullable = false)
+    private Integer quantityInStock;
+
+
+}
+
