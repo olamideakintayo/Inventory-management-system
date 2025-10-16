9a44b0772ad9058d52b350dddaadb575074686ad
olamideakintayo <divineolamide977@gmail.com>
2025-10-16 17:50:40 +0100

    Added PurchaseOrder Mapper

---
+++ src/main/java/org/kashcode/inventorymanagementsystem/utils/PurchaseOrderMapper.java
@@ -0,0 +1,18 @@
+package org.kashcode.inventorymanagementsystem.utils;
+
+import org.kashcode.inventorymanagementsystem.data.models.PurchaseOrder;
+import org.kashcode.inventorymanagementsystem.dtos.responses.PurchaseOrderResponse;
+
+public class PurchaseOrderMapper {
+    public PurchaseOrderResponse toPurchaseOrderResponse(PurchaseOrder order) {
+        PurchaseOrderResponse response = new PurchaseOrderResponse();
+        response.setOrderId(order.getPurchaseOrderId());
+        response.setProductName(order.getProduct().getName());
+        response.setSupplierName(order.getSupplier().getName());
+        response.setWarehouseName(order.getWarehouse().getName());
+        response.setQuantityOrdered(order.getQuantityOrdered());
+        response.setOrderDate(order.getOrderDate());
+        response.setExpectedArrivalDate(order.getExpectedArrivalDate());
+        return response;
+    }
+}
