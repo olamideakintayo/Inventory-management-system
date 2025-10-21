aeea1cedda1f9ad239da917bb4ee62462fc5c91e
olamideakintayo <divineolamide977@gmail.com>
2025-10-21 04:55:20 +0100

    Added SupplierService Impl

---
+++ src/main/java/org/kashcode/inventorymanagementsystem/services/SupplierServiceImpl.java
@@ -0,0 +1,44 @@
+package org.kashcode.inventorymanagementsystem.services;
+
+import org.kashcode.inventorymanagementsystem.data.models.Supplier;
+import org.kashcode.inventorymanagementsystem.data.repositories.SupplierRepository;
+import org.kashcode.inventorymanagementsystem.dtos.requests.SupplierRequest;
+import org.kashcode.inventorymanagementsystem.dtos.responses.SupplierResponse;
+import org.kashcode.inventorymanagementsystem.exceptions.SupplierNotFoundException;
+import org.kashcode.inventorymanagementsystem.utils.SupplierMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SupplierServiceImpl implements SupplierService {
+
+    private final SupplierRepository supplierRepository;
+
+    public SupplierServiceImpl(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    @Override
+    public SupplierResponse createSupplier(SupplierRequest request) {
+        Supplier supplier = SupplierMapper.toSupplierEntity(request);
+
+        Supplier savedSupplier = supplierRepository.save(supplier);
+        return SupplierMapper.toSupplierResponse(savedSupplier);
+    }
+
+    @Override
+    public List<SupplierResponse> getAllSuppliers() {
+        return supplierRepository.findAll()
+                .stream()
+                .map(SupplierMapper::toSupplierResponse)
+                .toList();
+    }
+
+    @Override
+    public SupplierResponse getSupplierById(Long supplierId) {
+        Supplier supplier = supplierRepository.findById(supplierId)
+                .orElseThrow(() -> new SupplierNotFoundException("Supplier not found"));
+        return SupplierMapper.toSupplierResponse(supplier);
+    }
+}
