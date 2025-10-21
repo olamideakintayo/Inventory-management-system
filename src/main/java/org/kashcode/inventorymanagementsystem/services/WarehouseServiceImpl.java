9a7421ba8e53b7964b3d9b508b123122d3f77169
olamideakintayo <divineolamide977@gmail.com>
2025-10-21 04:48:44 +0100

    Added Warehouse service impl

---
+++ src/main/java/org/kashcode/inventorymanagementsystem/services/WarehouseServiceImpl.java
@@ -0,0 +1,44 @@
+package org.kashcode.inventorymanagementsystem.services;
+
+import org.kashcode.inventorymanagementsystem.data.models.Warehouse;
+import org.kashcode.inventorymanagementsystem.data.repositories.WarehouseRepository;
+import org.kashcode.inventorymanagementsystem.dtos.requests.WarehouseRequest;
+import org.kashcode.inventorymanagementsystem.dtos.responses.WarehouseResponse;
+import org.kashcode.inventorymanagementsystem.exceptions.WarehouseNotFoundException;
+import org.kashcode.inventorymanagementsystem.utils.WarehouseMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WarehouseServiceImpl implements WarehouseService {
+
+    private final WarehouseRepository warehouseRepository;
+
+    public WarehouseServiceImpl(WarehouseRepository warehouseRepository) {
+        this.warehouseRepository = warehouseRepository;
+    }
+
+    @Override
+    public WarehouseResponse createWarehouse(WarehouseRequest request) {
+        Warehouse warehouse = WarehouseMapper.toWarehouseEntity(request);
+
+        Warehouse savedWarehouse = warehouseRepository.save(warehouse);
+        return WarehouseMapper.toWarehouseResponse(savedWarehouse);
+    }
+
+    @Override
+    public List<WarehouseResponse> getAllWarehouses() {
+        return warehouseRepository.findAll()
+                .stream()
+                .map(WarehouseMapper::toWarehouseResponse)
+                .toList();
+    }
+
+    @Override
+    public WarehouseResponse getWarehouseById(Long warehouseId) {
+        Warehouse warehouse = warehouseRepository.findById(warehouseId)
+                .orElseThrow(() -> new WarehouseNotFoundException("Warehouse not found"));
+        return WarehouseMapper.toWarehouseResponse(warehouse);
+    }
+}
--- src/main/java/org/kashcode/inventorymanagementsystem/utils/WarehouseMapper.java
+++ src/main/java/org/kashcode/inventorymanagementsystem/utils/WarehouseMapper.java
@@ -5,7 +5,7 @@ import org.kashcode.inventorymanagementsystem.dtos.requests.WarehouseRequest;
 import org.kashcode.inventorymanagementsystem.dtos.responses.WarehouseResponse;
 
 public class WarehouseMapper {
-    public Warehouse toWarehouseEntity(WarehouseRequest request) {
+    public static Warehouse toWarehouseEntity(WarehouseRequest request) {
         Warehouse warehouse = new Warehouse();
         warehouse.setName(request.getName());
         warehouse.setAddress(request.getAddress());
@@ -13,7 +13,7 @@ public class WarehouseMapper {
         return warehouse;
     }
 
-    public WarehouseResponse toWarehouseResponse(Warehouse warehouse) {
+    public static WarehouseResponse toWarehouseResponse(Warehouse warehouse) {
         WarehouseResponse response = new WarehouseResponse();
         response.setWarehouseId(warehouse.getWarehouseId());
         response.setName(warehouse.getName());
