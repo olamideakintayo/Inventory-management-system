3faa4b2da86023c3590b9ce9a1e61bdcbdee9036
olamideakintayo <divineolamide977@gmail.com>
2025-10-21 04:50:33 +0100

    Added SupplierService Interface

---
+++ src/main/java/org/kashcode/inventorymanagementsystem/services/SupplierService.java
@@ -0,0 +1,12 @@
+package org.kashcode.inventorymanagementsystem.services;
+
+import org.kashcode.inventorymanagementsystem.dtos.requests.SupplierRequest;
+import org.kashcode.inventorymanagementsystem.dtos.responses.SupplierResponse;
+
+import java.util.List;
+
+public interface SupplierService {
+    SupplierResponse createSupplier(SupplierRequest request);
+    List<SupplierResponse> getAllSuppliers();
+    SupplierResponse getSupplierById(Long supplierId);
+}
