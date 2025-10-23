dd906479e6e009cfbe48512fe5737825cefc3b2f
olamideakintayo <divineolamide977@gmail.com>
2025-10-23 20:38:59 +0100

    Added Warehouse Controller

--- src/main/java/org/kashcode/inventorymanagementsystem/controllers/WarehouseController.java
+++ src/main/java/org/kashcode/inventorymanagementsystem/controllers/WarehouseController.java
@@ -6,10 +6,9 @@ import org.kashcode.inventorymanagementsystem.dtos.responses.WarehouseResponse;
 import org.kashcode.inventorymanagementsystem.services.WarehouseService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/warehouses")
@@ -22,9 +21,33 @@ public class WarehouseController {
     }
 
     @PostMapping
-    public ResponseEntity<WarehouseResponse> createWarehouse(@Valid @RequestBody WarehouseRequest warehouse) {
-        WarehouseResponse savedWarehouse = warehouseService.createWarehouse(warehouse);
-        return new ResponseEntity<>(savedWarehouse,HttpStatus.CREATED);
+    public ResponseEntity<WarehouseResponse> createWarehouse(@Valid @RequestBody WarehouseRequest request) {
+        WarehouseResponse savedWarehouse = warehouseService.createWarehouse(request);
+        return new ResponseEntity<>(savedWarehouse, HttpStatus.CREATED);
+    }
 
+    @GetMapping
+    public ResponseEntity<List<WarehouseResponse>> getAllWarehouses() {
+        return ResponseEntity.ok(warehouseService.getAllWarehouses());
+    }
+
+    @GetMapping("/{warehouseId}")
+    public ResponseEntity<WarehouseResponse> getWarehouseById(@PathVariable Long warehouseId) {
+        WarehouseResponse response = warehouseService.getWarehouseById(warehouseId);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{warehouseId}")
+    public ResponseEntity<WarehouseResponse> updateWarehouse(
+            @PathVariable Long warehouseId,
+            @Valid @RequestBody WarehouseRequest request) {
+        WarehouseResponse response = warehouseService.updateWarehouse(warehouseId, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{warehouseId}")
+    public ResponseEntity<String> deleteWarehouse(@PathVariable Long warehouseId) {
+        warehouseService.deleteWarehouse(warehouseId);
+        return ResponseEntity.ok("Warehouse deleted successfully");
     }
 }
